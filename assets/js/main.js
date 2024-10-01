c7e3851baf28f181c040bfb99187d96264bed93a
Fabio Eizo Matsumoto <fabioe.matsumoto@gmail.com>
2024-10-01 14:41:13 -0300

    começando a mexer no js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,13 @@
-console.log("Sucesso!");
+const offset = 0
+const limit = 10
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+
+fetch(url).then(function (response) {
+    console.log(response)
+})
+
+
+
+const x = 10 + 10
+console.log(x)
