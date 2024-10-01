d7840efe846678043d5b9e18150eaebe2ad218c3
Fabio Eizo Matsumoto <fabioe.matsumoto@gmail.com>
2024-10-01 19:42:28 -0300

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,9 +3,13 @@ const limit = 10
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
 
-fetch(url).then(function (response) {
-    console.log(response)
-})
+fetch(url)
+    .then((response) => response.json())   // funcao com sintaxe reduzida // retorna uma promessa
+    .then((jsonBody) => console.log(jsonBody)) // ja vou receber o meu body convertido
+    .catch((error) => console.log(error))
+    .finally(function () {
+        console.log('Requisição concluída!')
+    })
 
 
 
