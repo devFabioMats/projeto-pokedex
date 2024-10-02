e0ea5b4dda4c1035c3df370cbebf71dcebd596d4
Fabio Eizo Matsumoto <fabioe.matsumoto@gmail.com>
2024-10-02 16:24:37 -0300

    update

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,15 +1,18 @@
-
-
-
 const pokeApi = {};
+
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url).then((responde) => responde.json())
+}
 
 pokeApi.getPokemons = function (offset = 0, limit = 10) {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
-    return fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
-    .catch((error) => console.log(error));
-}
 
+    return fetch(url)
+        .then((response) => response.json())
+        .then((jsonBody) => jsonBody.results)
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+        .then((detailRequests) => Promise.all(detailRequests))
+        .then((pokemonsDetails) => pokemonsDetails)
+}   
 
 
