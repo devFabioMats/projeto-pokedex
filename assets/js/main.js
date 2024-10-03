2aea63cfd32266bfc33d7662554463810864bcfa
Fabio Eizo Matsumoto <fabioe.matsumoto@gmail.com>
2024-10-03 12:30:27 -0300

    Correção: pokemon ssumido

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,6 @@
-const limit = 10
-let offset = 0;
-
 function convertPokemonToLi(pokemon) {
     return `
-        <li class="pokemon ${pokemon.type}">
+        <li class="pokemon">
             <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
 
@@ -12,8 +9,7 @@ function convertPokemonToLi(pokemon) {
                     ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
                 </ol>
 
-                <img src="${pokemon.photo}"
-                     alt="${pokemon.name}">
+                <img src="${pokemon.photo}" alt="${pokemon.name}">
             </div>
         </li>
     `
